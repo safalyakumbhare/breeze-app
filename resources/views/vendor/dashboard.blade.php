e08a0be4282e48a939b748f6545e3faf7a5eacbc
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-12-06 12:36:40 +0530

    first commit

---
+++ resources/views/vendor/dashboard.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Vendor</h1>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <x-responsive-nav-link :href="route('logout')"
+            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+            {{ __('Log Out') }}
+        </x-responsive-nav-link>
+    </form>
+</body>
+
+</html>
